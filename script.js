17ed0cd56e0155c1a7de1b35c967428ef7a835eb
natebrant <39286238+natebrant@users.noreply.github.com>
2022-06-22 13:16:02 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var x = 0;
 function run() {
 
     document.getElementById("test").style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ","+Math.floor(Math.random() * 255)+")";
-    console.log("rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ","+Math.floor(Math.random() * 255)+")");
-    setTimeout(run,1);
+document.getElementById("test").style.height = (x+=.1)+"px";
+    document.getElementById("test").style.width = (x+=.01)+"px";
+     document.getElementById("test").style.marginTop = (x+=.1)+"px";
+    document.getElementById("test").style.marginLeft = (x+=.1)+"px";
+    document.getElementById("test").style.marginLeft = -(Math.floor(Math.random() * 50))+"px";
+        document.getElementById("test").style.marginTop = -(Math.floor(Math.random() * 50))+"px";
+    x+=1;
+ 
+    setTimeout(run,100);
 }
